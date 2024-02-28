d1943f6e87d7a1d980bfa0397fd298dbb6e60c07
Luukas <78479063+luukasmepham@users.noreply.github.com>
2024-02-28 17:52:44 +0200

    Functional tiles

--- links/links_app/urls.py
+++ links/links_app/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:category_id>/', views.detail, name='detail'),
-    path('<int:category_id>/word_options/', views.results, name='results'),
 ]
